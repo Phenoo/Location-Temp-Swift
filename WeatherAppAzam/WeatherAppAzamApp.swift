9b7030b6091dfdd85680ba1222056fda4e0f9495
Phenoo <descometusah@gmail.com>
2024-07-26 21:24:22 +0100

    Initial Commit

---
+++ WeatherAppAzam/WeatherAppAzamApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppAzamApp.swift
+//  WeatherAppAzam
+//
+//  Created by Eze Chidera Paschal on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppAzamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
